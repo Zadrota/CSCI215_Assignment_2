52e17d48b9af42c32b3c26170a7719637720c070
zadrota <m.zach.dayton@gmail.com>
2017-10-11 22:54:40 -0400

    trying proc.js again, still needs love.

--- js/proc.js
+++ js/proc.js
@@ -7,39 +7,23 @@ function countwords() {
 
     var numWords = 1;
     var textfreq = [1];
-    //console.log(textsplit);
-    /*textsplit.sort();
-    for(i=1;i<textsplit.length;i++){
-        numWords++;
-    }
-    //console.log(numWords);
-    for(i=1;i<=textsplit.length;i++) {
-        if (textsplit[i] == textsplit[i+1]) {
-            textfreq[i]= textfreq[i]+1;
-            console.log(textfreq);
-        } else if(textsplit[i] != textsplit[i+1]){
-            textfreq.push(1);
-        }
-    }
-    display(textsplit, textfreq);*/
+    //console.log(textfreq);
     var processedText = [];
     for (i=0;i<textsplit.length;i++) {
-        if (processedText.indexOf(textsplit[i]) > -1){
+        if (textsplit[i] === textsplit[i-1]){
             textfreq[i] += 1;
+            //processedText[i]+=1;
         }else{
             textfreq.push(1);
             processedText.push(textsplit[i]);
         }
     }
     console.log(processedText);
-    document.getElementById("output").innerHTML=processedText;
-}
-function display(split,freq){
-    /*var processedText = []
-    for(i=0;i<split.length;i++){
-        processedText.push(split[i]);
-        processedText.push(freq[i]);
+    fin = [];
+    for(i=0; i<processedText;i++){
+        fin.push(processedText[i] + textfreq[i]);
+        console.log(fin);
+    }
 
-    }*/
-
+    //document.getElementById("output").innerHTML= fin;
 }
